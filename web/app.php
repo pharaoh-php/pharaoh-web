3eb37274fdc107a13803bf40e61947319cb9c6ff
Steve Buzonas <steve@fancyguy.com>
2016-08-28 19:18:18 -0400

    better env handling

--- web/app.php
+++ web/app.php
@@ -1,18 +1,37 @@
 <?php
 
+use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @var Composer\Autoload\ClassLoader
  */
 $loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
 
-$kernel = new PharaohKernel('prod', false);
+$env   = getenv('SYMFONY_ENV')   ?: 'stage';
+$debug = getenv('SYMFONY_DEBUG') ? true : false;
+
+if ($debug && class_exists('Symfony\Component\Debug\Debug')) {
+    Debug::Enable();
+}
+
+if ('prod' === $env) {
+    include_once __DIR__.'/../var/bootstrap.php.cache';
+
+    /*
+    $apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
+    $loader->unregister();
+    $apcLoader->register(true);
+    */
+}
+
+$kernel = new PharaohKernel($env, $debug);
 $kernel->loadClassCache();
-$kernel = new PharaohCache($kernel);
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+if ('prod' === $env) {
+    $kernel = new PharaohCache($kernel);
+}
+
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
